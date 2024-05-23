e8c474dec99506a90530c044d0d0ee2d7fb49715
praskouya <248656@edu.p.lodz.pl>
2024-05-23 12:55:17 +0300

    11.Dokonano hermetyzacji

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,10 +2,38 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    public int value;
-    public Node prev, next;
+    private int value;
+    private Node prev, next;
+    
+    public Node (int value) {
+        this.value = value;
+    }
 
-    public Node(int i) {
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+     public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    private Node(int i) {
         value = i;
     }
     
