6469bd1a4bd6d060122d50833bc4368a4905a0a9
praskouya <248656@edu.p.lodz.pl>
2024-05-23 12:56:16 +0300

    12.Usunieto niepotrzebne settery

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -13,25 +13,15 @@ public class Node {
         return value;
     }
 
-    public void setValue(int value) {
-        this.value = value;
-    }
-
      public Node getPrev() {
         return prev;
     }
 
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
 
     public Node getNext() {
         return next;
     }
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
 
     private Node(int i) {
         value = i;
